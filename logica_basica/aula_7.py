6ab01906554f68c3d9ce405e8422e1e68c1fda46
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-13 20:55:55 -0300

    Adiciona conteúdo de aula 7 de lógica báscia em Python, sobre uso de variáveis

---
+++ logica_basica/aula_7.py
@@ -0,0 +1,10 @@
+# Variáveis
+
+"""
+Servem para armazenarem dados na memória do computador
+
+PEP8 -> inicie a nomenclatura de variáveis com letras minusculas, números e _
+
+O sinal '=' serve para atribuir um valor à uma variável. Ex.: nome_aluno = 'Pedro'
+"""
+nome_completo = 'Pedro Otávio de Sousa Bezerra'
