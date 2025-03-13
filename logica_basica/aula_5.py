6c81725b025634466d00eaa9507f70b381a9eb08
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-13 16:29:19 -0300

    Adiciona aula 5 de lógica básica em Python, sobre o tipo de dado boolean e o operador lógico '=='

---
+++ logica_basica/aula_5.py
@@ -0,0 +1,13 @@
+"""
+Tipo de dado: bool (boolean)
+Possui apenas 2 valores possíveis: true (Verdadeiro) e false (Falso)
+
+Operadores Lógicos
+Existem vários tipos de operadores que trabalham muito bem com dados, retornando valores booleanos
+Dentre eles, existe o operador "==". Este, seria uma espécie de igual, retornando true ou false 
+dependendo dos dados comparados
+"""
+print(10 == 10)
+print(50 == 5)
+print(50 == '50')
+print('' == True)
