e7540fae5bc55f89199755d1781bd1963bfc5eab
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-13 15:52:02 -0300

    Adiciona aula 2 de python, sobre a função print e alguns de seus argumentos

---
+++ logica_basica/aula_2.py
@@ -0,0 +1,25 @@
+# Função print()
+
+"""
+Usada para exibir comandos na tela do computador
+Recebe argumentos para serem exibidos, sendo separados por ','
+"""
+
+print('Olá, Mundo!')
+
+# Argumentos extras
+
+"""
+sep
+Usado quando para separar os argumentos, separados por virgula na função print()
+"""
+
+print(12, 3, 2025, sep='/') # Printa na tela a data atual
+
+"""
+end
+Argumento que coloca na tela um determinado argumento, ao final da linha
+"""
+
+print('Valor de salário recebido', end=':\n') # coloca os ':' e pula a linha
+print('R$', 5580.00, sep=' ')
