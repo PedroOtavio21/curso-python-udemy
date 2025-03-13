2dd49504d1cad30c7c24666b93c79c57d0155fa1
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-13 20:54:26 -0300

    Adiciona conteúdo de aula 6 de lógica básica em Python, sobre conversão de dados

---
+++ logica_basica/aula_6.py
@@ -0,0 +1,17 @@
+"""
+Conversão de Dados em Python (tipos, coerção)
+
+type convertion, typecasting, coercion é o ato de converter um tipo de dado em outro
+
+tipos imutáveis e primitivos:
+str, int, float, bool
+"""
+
+print('a' + 'b')
+print(1 + 2)
+# print(3 + '3') # erro
+
+print(int('1') + 1)
+print(float('1.2') + 1)
+print(bool(' '))
+print(str(11) + 'b')
