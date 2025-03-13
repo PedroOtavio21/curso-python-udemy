ce43ab9d9b9252059e7bdf077b2b90a1b8feb13f
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-13 16:04:38 -0300

    Adiciona aula 3 de lógica básica em python, sobre o tipo de dado string

---
+++ logica_basica/aula_3.py
@@ -0,0 +1,21 @@
+# Tipos de dados em Python
+
+"""
+Python -> Linguagem de Programação
+Tipo de Tipagem -> Dinâmica / Forte
+
+str -> String -> Dado de texto
+São dados de texto que se encontram dentro de aspas
+"""
+
+# Aspas simples
+print('Olá, Mundo!')
+
+# Aspas duplas
+print("Meu nome é Pedro.")
+
+# Escape 
+print('\"Programador Treinee\"') # Geralmente é qualquer dado seguido do '\'. (Ex.: \, /, ', ")
+
+# r
+print(r'\"Programador Treinee\"') # Formatador do print, além de ser usado para expressões regulares
