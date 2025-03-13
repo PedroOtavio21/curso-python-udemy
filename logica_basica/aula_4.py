a9b4de732e6847c44f094bcb11d519540208d8b7
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-13 16:28:29 -0300

    Adiciona aula 4 de lógica básica em Python, sobre os tipos de dados int e float, além do método type()

---
+++ logica_basica/aula_4.py
@@ -0,0 +1,15 @@
+"""
+Tipos int e float
+
+int -> Número inteiro
+Este tipo representa qualquer número inteito negativo ou positivo
+
+float -> Número com ponto flutuante 
+Representa qualquer número de ponto flutuante sendo positivo ou negativo
+
+type() -> Função responsável por postar o tipo de um dado
+
+OBS.: Tudo em Python é uma Classe!
+"""
+
+print('Tipos de dados de 1 e 3.14 = ', type(1), type(3.14))
