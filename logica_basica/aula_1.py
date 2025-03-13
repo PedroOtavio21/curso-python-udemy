3383b9d956d682e21b5fe8cc6db3097e24b89874
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-13 15:10:04 -0300

    Adiciona primeiro commit em repositório de estudos de python, pela udemy

---
+++ logica_basica/aula_1.py
@@ -0,0 +1,7 @@
+# Comentários em Python
+# Detectados pelo '#', fazendo com oque a saída do comando na linha não seja exibido
+# Além disso, poderá documentar uma minha de código
+
+# Comando print()
+# Exibe um determinado comando no prompt do console
+print('Olá, mundo!')
