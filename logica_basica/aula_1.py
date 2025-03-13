7c72c8f1569b5ee1893fe64122564ec3b5fcf2b3
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-13 15:50:23 -0300

    Atualiza conteúdo de aula 1 em Python, sobre diferenças dos tipos de comentários em python

--- logica_basica/aula_1.py
+++ logica_basica/aula_1.py
@@ -1,7 +1,16 @@
 # Comentários em Python
-# Detectados pelo '#', fazendo com oque a saída do comando na linha não seja exibido
+# Detectados pelo '#', fazendo com oque a saída do comando na linha não seja exibido (ignorada)
 # Além disso, poderá documentar uma minha de código
 
-# Comando print()
-# Exibe um determinado comando no prompt do console
-print('Olá, mundo!')
+# DocStrings
+# Similar aos comentários multi-linha em outras linugagens de programação, porém NÃO É UM COMENTÁRIOS
+# Ótimo para documentar trechos de código
+
+"""
+Este é um comentário multi-linha no python
+Pode ser usado um enter para pular linha
+"""
+
+'''
+Pode ser usado com aspas simples!
+'''
