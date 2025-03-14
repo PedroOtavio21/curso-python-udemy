99da7fa5396f42131bd7cd7d497bc10a4b759271
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-14 10:38:54 -0300

    Adiciona conteúdo de aula 9 em lógica básica, sobre os principais operadores aritméticos

---
+++ logica_basica/aula_9.py
@@ -0,0 +1,28 @@
+adicao = 9 + 9
+print('Adição', adicao)
+
+subtracao = 9 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 9 * 2
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2 # Sempre com retorno float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2 
+print('Divisão Inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 25 % 5 # Resto da Divisão
+print('Módulo', modulo)
+
+# É divisível? 
+print(10 % 5 == 0) # true
+print(9 % 2 == 0)
+
+# É par? 
+print('6 é Par?', (6 % 2 == 0))
+print('15 é Par?', (15 % 2 == 0))
