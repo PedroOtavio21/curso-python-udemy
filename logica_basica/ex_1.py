0ab5953b42a086e52595c607095c8fb16d33a0e7
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-14 10:28:37 -0300

    Adiciona primeiro exercício de lógica básica em python, BEM SIMPLES

---
+++ logica_basica/ex_1.py
@@ -0,0 +1,13 @@
+nome = 'Pedro'
+sobrenome = 'Otávio'
+idade = 22
+ano_nascimento = 2002
+maior_idade = idade >= 18
+altura_metros = 1.74
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em Metros:', altura_metros)
