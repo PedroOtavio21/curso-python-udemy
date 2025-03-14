49a42a0a9a11dd0f0569a3016af1bcfac9bd85a1
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-14 10:22:09 -0300

    Atualiza conteúdo de aula 7 de lógica básica em python, contendo exemplos de código restantes

--- logica_basica/aula_7.py
+++ logica_basica/aula_7.py
@@ -8,3 +8,9 @@ PEP8 -> inicie a nomenclatura de variáveis com letras minusculas, números e _
 O sinal '=' serve para atribuir um valor à uma variável. Ex.: nome_aluno = 'Pedro'
 """
 nome_completo = 'Pedro Otávio de Sousa Bezerra'
+idade = 22
+maior_idade = idade >= 18 
+
+print('Nome completo:', nome_completo)
+print('Idade do usuário:', idade)
+print('É maior de idade?', maior_idade)
