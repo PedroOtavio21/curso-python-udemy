64b84ec38bc3ccafe0ebeea576250f7b0ad1db8c
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-03-15 10:21:16 -0300

    Adiciona aula 10 de lógica básica em Python, sobre concatenação e repetição de valores

---
+++ logica_basica/aula_10.py
@@ -0,0 +1,9 @@
+# Concatenação
+concatenacao = 'A' + 'B' + 'C'
+print('Concatenação:', concatenacao)
+
+# Repetição
+a_dez_vezes = 'A' * 10
+tres_vezes_pedro = 'Pedro' * 3
+print(a_dez_vezes)
+print(tres_vezes_pedro)
